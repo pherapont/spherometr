cf36b329035b9f4a4f0897de3e2af1f86fcceef9
pherapont <56246562+pherapont@users.noreply.github.com>
2020-12-18 07:04:39 +0700

    first test

--- Controller/height_test.py
+++ Controller/height_test.py
@@ -1,9 +1,16 @@
 import unittest
 
-class TestHeight(unittest.TestCase):
+from Model.data import SPHEROMETR
+from Controller.calculator import concave_height
+
+
+class TestConcaveHeight(unittest.TestCase):
 
     def test_concave_100(self):
-        self.assertEqual(50 + 50, 100, "Should be 100")
+        type = SPHEROMETR["BIG"]
+        ring = SPHEROMETR["RING_1"]
+        self.assertEqual(concave_height(1000, type, ring), 1.590, "It the test")
+
 
 if __name__ == '__main__':
     unittest.main()
